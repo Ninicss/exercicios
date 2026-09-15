77818042eb8215127cfa13ed20bc2af96076bdd6
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:25:04 -0300

    nivel 4 exercicio 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o valor do raio do círculo:");
-        double raio = entrada.nextDouble();
-        double area = Math.PI * (raio * raio);
-        System.out.println("Área do círculo:\n" + area);
+        System.out.println("Digite o valor de A:");
+        int a = entrada.nextInt();
+        System.out.println("Digite o valor de B:");
+        int b = entrada.nextInt();
+
+        System.out.println("Valores antes da troca:\nA: " + a + "\nB: " + b);
+
+        int aux = a;
+        a = b;
+        b = aux;
+
+        System.out.println("Valores depois da troca:\nA: " + a + "\nB: " + b);
     }
 }
