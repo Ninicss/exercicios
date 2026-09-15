4cddbd16f1d25ca555c9b3dc82f9a953e4fc0665
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:53:03 -0300

    repetição 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,13 @@ package org.example;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        for (int peca = 1; peca <= 20; peca++) {
-            System.out.println("Peça Produzida");
+        Scanner entrada = new Scanner(System.in);
+        double total = 0;
+        for (int venda = 1; venda <= 5; venda++) {
+            System.out.println("Digite o valor da venda " + venda);
+            double valor = entrada.nextDouble();
+            total += valor;
         }
+        System.out.println("Faturamento total do dia: R$ " + total);
     }
 }
