934add2df158ed00f6fd72eae4840424709a7fad
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:42:56 -0300

    decisao 9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.print("Você possui veículo próprio? (sim/nao): ");
-        String resposta  = entrada.nextLine();
-
-        if (resposta.equalsIgnoreCase("Sim")) {
-            System.out.println("Auxílio combustível disponível.");
+        System.out.println("Digite o cargo do funcionário:");
+        String cargo = entrada.nextLine();
+        if (cargo.equalsIgnoreCase("administrativo") || cargo.equalsIgnoreCase("liderança") || cargo.equalsIgnoreCase("lideranca")) {
+            System.out.println("Você pode participar do programa de idiomas.");
         } else {
-            System.out.println("Não possui auxílio combustível.");
+            System.out.println("Você não tem acesso ao programa de idiomas no momento.");
         }
     }
 }
