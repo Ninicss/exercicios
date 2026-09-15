b8116a5b68bcb2bbff227a457fb06cbf43423a19
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 14:45:39 -0300

    nivel 2 exercicio 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        short valorShort = 32;
-        int valorInt = valorShort;
-        byte valorByte = (byte) valorShort;
+        int a, b;
+        a = 20;
+        b = 5;
+        boolean maior = a > b;
 
-        System.out.println("Valor short: " + valorShort);
-        System.out.println("Valor int: " + valorInt);
-        System.out.println("Valor byte: " + valorByte);
-
-
-
+        System.out.println("a maior que b? R: " + maior);
+        System.out.println("a menor que b? R: " + (a < b));
+        System.out.println("a igual a b? R: " + (a == b));
+        System.out.println("a diferente de b? R: " + (a != b));
+        System.out.println("a maior ou igual a b? R: " + (a >= b));
+        System.out.println("a menor ou igual a b? R: " + (a <= b));
     }
 }
