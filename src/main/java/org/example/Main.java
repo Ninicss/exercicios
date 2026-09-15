a1c1878c93c43f95a5efec49aa901e9249a23e2e
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 14:51:07 -0300

    nivel 3 exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite a primeira nota: (10 a 0)");
-        double nota1= sc.nextDouble ();
-        System.out.println("Digite a segunda nota: (10 a 0)");
-        double nota2= sc.nextDouble ();
-        System.out.println("Digite a terceira nota: (10 a 0)");
-        double nota3= sc.nextDouble ();
-        double media = (nota1 + nota2 + nota3)/ 3;
-        System.out.println ("média:\n" + media);
+        System.out.println("Digite a temperatura em °C");
+        double celsius= sc.nextDouble ();
+        double faren = (celsius*9/5) + 32;
+        System.out.println ("A temperatura em farenheit:\n" + faren + " °F");
     }
 }
