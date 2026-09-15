cb618b8ee372dbb02188df5e82285ac8b38abfea
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 14:47:02 -0300

    nivel 2 exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,19 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        int a, b;
-        a = 20;
-        b = 5;
-        boolean maior = a > b;
+        boolean A = true;
+        boolean B = false;
+        boolean cond1, cond2, cond3;
 
-        System.out.println("a maior que b? R: " + maior);
-        System.out.println("a menor que b? R: " + (a < b));
-        System.out.println("a igual a b? R: " + (a == b));
-        System.out.println("a diferente de b? R: " + (a != b));
-        System.out.println("a maior ou igual a b? R: " + (a >= b));
-        System.out.println("a menor ou igual a b? R: " + (a <= b));
+        cond1 = A && B;
+        cond2 = A || B;
+        cond3 = !A;
+
+        System.out.println("A E B: " + cond1);
+        System.out.println("A OU B: " + cond2);
+        System.out.println("Inverso de A: " + cond3);
+
+
+
     }
 }
