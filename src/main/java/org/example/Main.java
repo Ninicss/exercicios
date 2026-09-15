71754f4009bb3930ad35ec6779c82170ee9c77e9
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:22:30 -0300

    nivel 4 exercicio 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite a distância percorrida em km:");
-        double dist = entrada.nextDouble();
-        System.out.println("Digite o total de combustível gasto em litros:");
-        double litros = entrada.nextDouble();
-        double consumo = dist / litros;
-        System.out.println("Consumo médio:\n" + consumo + " km/l");
+        System.out.println("Digite o valor da hora trabalhada:");
+        double valorHora = entrada.nextDouble();
+        System.out.println("Digite o número de horas trabalhadas no mês:");
+        double horas = entrada.nextDouble();
+        double salario = valorHora * horas;
+        System.out.println("Salário bruto mensal:\n" + "R$ " + salario);
     }
 }
