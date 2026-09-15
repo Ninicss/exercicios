7e383e3203dd187b8e4574d38a793187afc22d16
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:18:50 -0300

    nivel 3 exercicio 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o valor da base em metros:");
-        double base = entrada.nextDouble();
-        System.out.println("Digite o valor da altura em metros:");
-        double altura = entrada.nextDouble();
-        double peri = base + altura + base + altura;
-        System.out.println("Perímetro do retângulo:\n" + peri + " m");
+        System.out.println("Digite o valor do produto:");
+        double prod = entrada.nextDouble();
+        System.out.println("Digite a porcentagem de desconto:");
+        double desc = 0.15;
+        double valorReal = prod - (prod * desc);
+        System.out.println("Preço do produto com desconto:\n" + "R$" + valorReal);
     }
 }
