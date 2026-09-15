879d83facd73943ccd1fa1121de20bbe389637c0
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:28:11 -0300

    nivel 5 exercicio 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o valor em metros:");
-        double metros = entrada.nextDouble();
-        double cm = metros * 100;
-        double mm = metros * 1000;
-        System.out.println("Valor convertido:\n" + cm + " cm\n" + mm + " mm");
+        System.out.println("Digite o capital inicial:");
+        double cap = entrada.nextDouble();
+        System.out.println("Digite a taxa de juros mensal (em %):");
+        double taxa = entrada.nextDouble();
+        System.out.println("Digite o tempo em meses:");
+        double tempo = entrada.nextDouble();
+
+        double juros = cap * (taxa / 100) * tempo;
+        double montante = cap + juros;
+
+        System.out.println("Montante final:\n" + "R$ " + montante);
     }
 }
