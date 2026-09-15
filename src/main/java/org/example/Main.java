89a2500fd9f7155f4e8d46f484d169ae7a06025b
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:33:17 -0300

    nivel 5 exercicio 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o capital inicial:");
-        double cap = entrada.nextDouble();
-        System.out.println("Digite a taxa de juros mensal (em %):");
-        double taxa = entrada.nextDouble();
-        System.out.println("Digite o tempo em meses:");
-        double tempo = entrada.nextDouble();
 
-        double juros = cap * (taxa / 100) * tempo;
-        double montante = cap + juros;
+        System.out.println("Digite a quantidade total de segundos:");
+        int totalSegundos = entrada.nextInt();
+        int horas = totalSegundos / 3600;
+        int resto = totalSegundos % 3600;
+        int minutos = resto / 60;
+        int segundos = resto % 60;
 
-        System.out.println("Montante final:\n" + "R$ " + montante);
+        System.out.println(horas + " horas, " + minutos + " minutos e " + segundos + " segundos");
     }
 }
