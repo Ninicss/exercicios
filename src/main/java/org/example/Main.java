87a0585117f10397968b238d69397a48019f548e
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:26:45 -0300

    nivel 5 exercicio 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,13 @@
 package org.example;
 
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite o peso em kg:");
-        double peso = entrada.nextDouble();
-        System.out.println("Digite a altura em metros:");
-        double altura = entrada.nextDouble();
-        double imc = peso / (altura * altura);
-        System.out.println("Índice de Massa Corporal (IMC):\n" + imc);
+        System.out.println("Digite o valor em metros:");
+        double metros = entrada.nextDouble();
+        double cm = metros * 100;
+        double mm = metros * 1000;
+        System.out.println("Valor convertido:\n" + cm + " cm\n" + mm + " mm");
     }
 }
