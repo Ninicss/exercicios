630181ed0de54b41e6ddece1e5502a066b830569
Ninicss <nicolesilvadejesus78@gmail.com>
2026-09-15 15:42:16 -0300

    decisao 8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        System.out.print("Você possui veículo próprio? (sim/nao): ");
+        String resposta  = entrada.nextLine();
 
-        System.out.print("Digite o tempo de contratação (meses): ");
-        double tempo = entrada.nextDouble();
-
-
-        if (tempo >= 3) {
-            System.out.println("O plano de saúde  está disponível para você.");
+        if (resposta.equalsIgnoreCase("Sim")) {
+            System.out.println("Auxílio combustível disponível.");
         } else {
-            System.out.println("Serviço não disponível para você.");
+            System.out.println("Não possui auxílio combustível.");
         }
     }
 }
